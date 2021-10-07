74d5146292c9dc08c618e6518d729f3ce3c51881
GYJ <2354684632@qq.com>
2021-10-07 13:25:58 +0800

    a test

---
+++ getchar.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	printf("aaa");
+	printf("this is a test");
+	int a = 0;
+	for(int i = 0;i<=10;i++)
+	{
+		a+=i;
+	}
+	printf("%d",a);
+	return 0;
+}
